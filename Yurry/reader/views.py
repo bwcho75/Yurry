28f2d0c89f4a5f79590edec28ac54c23f6b402ab
terry.cho <bwcho75@gmail.com>
2013-11-25 00:49:46 +0900

    Add facebook group feed reader code. (initial) Enhancement required features - Flip feature is required. - Date - Action (Recommend) - Comment etc

--- Yurry/reader/views.py
+++ Yurry/reader/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.template  import Context, loader
 import datetime
+import requests
+import json
 
 def hello_reader(request):
     now = datetime.datetime.now()
@@ -14,7 +16,24 @@ def what_time_is_it(request):
     return HttpResponse(template.render(context))
 
 def group_home(request):
-    context = Context({})
+    # call facebook group feed
+    GROUP_ID='352245738171385'
+    ACCESS_TOKEN='CAACEdEose0cBAJYFZCZB1qk2paJZBPyZAnIn277U5TXWpAlSNguxPFQZAoxqaYZApQcRkrJYjziTIv9QhcugGZAZBNZB6J6GfOYxeZAZCOqfMtFzqSyHcQZCq0X4y9QjZCfSgHfdmyrAcVfaW53TKNSBZBi4m6GbQOwnprj3RVuQ6uTVNexPYwCaN3IoYWQ9F3oEYOe3qCOgVtyj4LcQZDZD'
+    feed = inqury_group_feed(GROUP_ID,ACCESS_TOKEN) 
+
+    for item in feed['data']:
+        print item['from']['name']
+    # send the json feed result to template thru context
+    context = Context({'f':feed['data']})
     template = loader.get_template("group_home.html")
+    # rendering
     return HttpResponse(template.render(context))
 #http://jhproject.tistory.com/37
+
+def inqury_group_feed(group_id,access_token):
+    url = 'https://graph.facebook.com/' + group_id +'/feed?access_token='+access_token
+    r = requests.get(url)
+    j = r.json()
+    return j
+    
+    
